f2b9e174d2e4f7b255fd35ef48886365485d8f52
KumaTea <36222458+KumaTea@users.noreply.github.com>
2020-11-20 13:31:22 +0800

    20201120 minor fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import base64
 import tweepy
 from datetime import datetime
-from tweepy.error import TweepError
+from tweepy.error import TweepError, RateLimitError
 
 
 def read_file(filename, encrypt=False):
@@ -18,6 +18,16 @@ def query_token(token_id):
     return read_file(f'token_{token_id}', True)
 
 
+def format_text(text, length=12):
+    l = len(text)
+    if l == length:
+        return text
+    elif l < length:
+        return text + (length-l)*' '
+    elif l > 12:
+        return text[:length-3] + '...'
+
+
 max_days = 30
 max_friends = 30
 like_power = 1
@@ -27,17 +37,20 @@ reply_power = 3*like_power
 twitter_token = json.loads(query_token('twitter'))
 token_auth = tweepy.OAuthHandler(twitter_token['consumer_key'], twitter_token['consumer_secret'])
 token_auth.set_access_token(twitter_token['access_token'], twitter_token['access_token_secret'])
-twi = tweepy.API(token_auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+# twi = tweepy.API(token_auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+twi = tweepy.API(token_auth)
 
 
 host_user = input('Please input your username: ')
 try:
     twi.get_user(host_user)
+    print('User OK.')
 except TweepError:
     exit('Invalid user.')
 
 
 friend_ids = twi.friends_ids()
+print('Friends count:', len(friend_ids))
 friends = {}
 for i in friend_ids:
     friends[i] = 0
@@ -47,20 +60,26 @@ now = datetime.now()
 
 
 timeline = []
-for tweet in tweepy.Cursor(twi.user_timeline, id=host_user).items():
-    if (now - tweet.created_at).days > max_days:
-        break
-    timeline.append(tweet)
+try:
+    for tweet in tweepy.Cursor(twi.user_timeline, id=host_user).items():
+        if (now - tweet.created_at).days > max_days:
+            break
+        timeline.append(tweet)
+except RateLimitError:
+    print('RateLimitError.')
 print('Timeline:', len(timeline))
 for tweet in timeline:
     if (reply_id := tweet.in_reply_to_user_id) and reply_id in friends:
         friends[reply_id] += reply_power*(max_days - (now - tweet.created_at).days)
 
 likes = []
-for tweet in tweepy.Cursor(twi.favorites, id=host_user).items():
-    if (now - tweet.created_at).days > max_days:
-        break
-    likes.append(tweet)
+try:
+    for tweet in tweepy.Cursor(twi.favorites, id=host_user).items():
+        if (now - tweet.created_at).days > max_days:
+            break
+        likes.append(tweet)
+except RateLimitError:
+    print('RateLimitError.')
 print('Likes:', len(likes))
 for tweet in likes:
     if (like_id := tweet.user.id) in friends:
@@ -80,9 +99,9 @@ max_rate = round(100*friends[first_friend_id]/total_score)
 
 html_start = f'<!DOCTYPE html><html><head> <meta charset="utf-8"> <title>Top Friends by @{host_user}</title> <meta ' \
              'name="viewport" content="width=device-width, initial-scale=1.0"> <style>img.avatar{border-radius: 50%; ' \
-             '}.container{position: relative; text-align: center; color: white; transform: rotate(' \
-             '90deg)}.info{position: absolute; top: 50%; left: 50%; transform: translate(-50%, ' \
-             '-50%);}</style></head><body><div>'
+             '}.container{position: relative; text-align: center; color: white; /*transform: rotate(' \
+             '90deg)*/}.info{position: absolute; top: 50%; left: 50%; transform: translate(-50%, ' \
+             '-50%);}</style></head><body><div class="container">'
 # opacity: 0.5;
 html_end = '</div></body></html>'
 
@@ -94,16 +113,15 @@ for i in friends:
     friend_info = twi.get_user(i)
     profile_url = friend_info.profile_image_url_https.replace('_normal', '')
     friend_rate = format(100*friends[i]/total_score, '.2f') + '%'
-    friend_html = '<div class="container">' \
-                  f'<img class="avatar" src="{profile_url}" ' \
-                  f'alt="Avatar" style="width:{round(240*friends[i]/friends[first_friend_id])}px"/>' \
-                  f'</div>'
+    friend_html = f'<img class="avatar" src="{profile_url}" ' \
+                  f'alt="Avatar" style="width:{round(240*friends[i]/friends[first_friend_id])}px"/>'
                   #f'<div class="info"><span style="color: black">{friend_info.name}</span><br><span style="color: black">{friend_info.screen_name}</span><br><progress max="{round(friends[first_friend_id])}" value="{round(friends[i]/friends[first_friend_id])}"></progress>&nbsp;&nbsp;<span>{friend_rate}</span></div>'
     html += friend_html
+    print(f'@{format_text(friend_info.screen_name)}\t{friend_rate}\t{friend_info.name}')
     index += 1
     if index > max_friends:
         break
 html += html_end
 
 with open('index.html', 'w', encoding='utf-8') as html_file:
-    f.write(html)
+    html_file.write(html)
