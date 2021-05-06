22ebf66c5f5569fd306fe890512743858e77ca6b
KumaTea <KumaTea@outlook.com>
2021-05-07 00:44:53 +0800

    update scripts

--- cron.sh
+++ cron.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
+echo "Generating date"
 DATE=$(date +%y%m%d --date="-1 day")
+TODAY=$(date +%y%m%d)
 
-# mkdir archive || :
-(mkdir -p "archive/$DATE" && mv index.html "archive/$DATE/" && mv more-info.html "archive/$DATE/" && cp gen.html index.html && cp gen.html more-info.html) || :
+echo "Backing up"
+mkdir archive || echo "Folder archive exists"
+mkdir -p "archive/$DATE" || echo "Folder archive/$DATE exists"
+mkdir -p "archive/$TODAY" || echo "Folder archive/$TODAY exists"
+mv index.html more-info.html "archive/$DATE/" || echo "Already backed up"
+cp gen.html index.html || echo "Already backed up"
+cp gen.html more-info.html || echo "Already backed up"
 
 echo "<meta http-equiv=\"refresh\" content=\"0; URL=$DATE/\" />" > "archive/index.html"
 
-pypy3 main.py || /usr/local/bin/pypy3 main.py
+echo "Executing main program"
+pypy3 main.py || /usr/local/bin/pypy3 main.py || /usr/local/bin/pypy3 main.py
 
-mv friends.p friends_info.p "archive/$DATE/"
+echo "Backing up"
+mv friends.p friends_info.p "archive/$TODAY/" || echo "Already backed up"
